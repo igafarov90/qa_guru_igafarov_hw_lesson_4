eda1b0da69f556649a9188b2a805626693c48e3e
igafarov <igafarov@dtln.ru>
2023-11-01 19:45:29 +0400

    search test: example junit5 code

---
+++ src/test/java/SelenideRepoTests.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideRepoTests {
+
+    @BeforeAll static void beforeAll() {
+        Configuration.holdBrowserOpen=true;
+        Configuration.baseUrl = "https://github.com/";
+    }
+    @Test
+    void searchExampleCodeForJunit5(){
+
+        open(baseUrl);
+        $(".search-input").click();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+        $("[data-testid='results-list']").$("span").click();
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $("[data-filterable-for='wiki-pages-filter']").shouldHave(text("SoftAssertions"));
+        $(byTagAndText("a","SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").sibling(0)
+                .shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"
+                ));
+    }
+}
